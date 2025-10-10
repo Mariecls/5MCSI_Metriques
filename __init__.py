9af83dfc49acf04f2f08fafdaec6df1920cb38b6
Mariecls <103645429+Mariecls@users.noreply.github.com>
2025-10-10 13:40:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,15 +3,17 @@ from urllib.request import urlopen
 import json
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')  # Assure-toi que hello.html est dans /templates
+    return render_template('hello.html')
+
 @app.route('/contact/')
 def ma_premiere_api():
     return "<h2>Ma page de contact</h2>"
+
 @app.route('/tawarano/')
 def meteo():
-    # Remplace 'xxx' par ta clé API si tu testes avec OpenWeatherMap réel
     url = 'https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx'
     response = urlopen(url)
     raw_content = response.read()
@@ -19,10 +21,15 @@ def meteo():
 
     results = []
     for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')  # timestamp UNIX
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin → °C
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
         results.append({'Jour': dt_value, 'temp': round(temp_day_value, 2)})
 
     return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
